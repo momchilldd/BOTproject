23c9353c18b1430aab127162c8d45b4611220fa6
momchilldd <66889211+momchilldd@users.noreply.github.com>
2023-06-12 21:13:10 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/horoscope', methods=['POST'])
+def horoscope():
+    date = request.form['date']
+    sign = request.form['sign']
+    response = requests.get(f'https://api.example.com/horoscope?date={date}&sign={sign}')  # Replace with a real horoscope API
+    horoscope_data = response.json()
+    return render_template('horoscope.html', horoscope=horoscope_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
